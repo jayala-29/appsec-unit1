108781ce1b5907a48ea10bdc4b5924c9ac5bfb21
Jessy Ayala <jessyayala@Jessys-iMac.local>
2020-09-09 16:09:03 -0700

    test travis with reorganization

--- test_main.c
+++ test_main.c
@@ -49,7 +49,7 @@ START_TEST(test_check_words_normal)
     expected[1] = "skyn";
     expected[2] = "betta";
     char *misspelled[MAX_MISSPELLED];
-    FILE *fp = fopen("test1.txt", "r");
+    FILE *fp = fopen("./txt_files/test1.txt", "r");
     int num_misspelled = check_words(fp, hashtable, misspelled);
     ck_assert(num_misspelled == 3);
     bool test = strlen(misspelled[0]) == strlen(expected[0]);
